b824a8bf3513175d086ffb6cac1ba1b3996a984c
BuildByRohan <rohanjamdade29@gmail,com>
2026-01-15 20:00:03 +0530

    solved

---
+++ Reverse Integer.py
@@ -0,0 +1,12 @@
+def reverse_integer(n):
+    sign = -1 if n < 0 else 1
+    n = abs(n)
+    rev = 0
+
+    while n > 0:
+        rev = rev * 10 + n % 10
+        n //= 10
+
+    return sign * rev
+
+
